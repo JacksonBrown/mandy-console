9f1ecbbafc1805e6578404c0c17a90f0f1ab8c20
JacksonBrown <jacksonconnerbrown@gmail.com>
2015-12-13 09:38:15 -0600

    AAAHHHHHHHH

--- bash-mandy/functions/open_pam.sh
+++ bash-mandy/functions/open_pam.sh
@@ -9,18 +9,32 @@ YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
 
 open_pam(){
-	printf "${YELLOW}Opening input pam file: \n1=common-auth, \n2=common-password, \n3=common-account \nother=enter the specified name \n${NC}"
+	printf "${YELLOW}Opening input pam file: \n1=common-auth, \n2=common-password, \n3=common-account, \nother=enter the specified name \n${NC}"
 
 	read option_pam
 
+	## ELIF OPT 1
 	if [[ $option_pam = 1 ]]; then
 		gedit /etc/pam.d/common-auth &
+
+	## ELIF OPT 2
 	elif [[ $option_pam = 2 ]]; then
 		gedit /etc/pam.d/common-password &
+
+	## ELIF OPT 3
 	elif [[ $option_pam = 3 ]]; then
 		gedit /etc/pam.d/common-account &
+
+	## ELIF OTHER
 	elif [ "$option_pam" = "other" ]; then
-		gedit /etc/pam.d/$option_pam &
+		printf "${YELLOW}Enter the specified pam file name${NC}: \n"
+		read pam_spec_option
+
+		if (gedit /etc/pam.d/$pam_spec_option &); then
+			echo "OK."
+		fi
+
+
 	else 
 		echo "Invalid spefication, closing open_pam"
 	fi
@@ -28,16 +42,8 @@ open_pam(){
 	## LOG UPDATER
 	echo >> $mandy_dir/logs/log.txt
 	echo "`date`" >> $mandy_dir/logs/log.txt
-	echo "View pam modules in MANDY, entry \"help\"." >> $mandy_dir/logs/log.txt
+	echo "View pam files via MANDY, entry \"openpam\"." >> $mandy_dir/logs/log.txt
 	echo "Commands Executed: cat" >> $mandy_dir/logs/log.txt
 	echo >> $mandy_dir/logs/log.txt
 }
-
-
-
-
-
-
-
-
 
--- bash-mandy/functions/replace_pam_files.sh
+++ bash-mandy/functions/replace_pam_files.sh
@@ -11,17 +11,17 @@ GREEN='\033[0;32m'
 source ../mconsole.sh
 
 replace_pam_files(){
-	echo "Would you like to replace the common-passswd file?y/n"
+	echo "Would you like to replace the common-password file?y/n"
 	read option_yes
 	if [ "$option_yes" = "y" ]; then
-		if (sudo cp $mandy_dir/textdocuments/common-password /etc/pam.d/); then
+		if (sudo cp $mandy_dir/misc/common-password /etc/pam.d/); then
 			echo "OK."
 		fi
 			
 		echo "Would you like to replace the login.def file?y/n"
 		read option_yes_2
 		if [ "$option_yes_2" = "y" ]; then
-			if (sudo cp $mandy_dir/textdocuments/login.defs /etc/); then
+			if (sudo cp $mandy_dir/misc/login.defs /etc/); then
 				echo "OK."
 			fi
 		fi
@@ -31,7 +31,7 @@ replace_pam_files(){
 	## LOG UPDATER
 	echo >> $mandy_dir/logs/log.txt
 	echo "`date`" >> $mandy_dir/logs/log.txt
-	echo "Replace pam files via MANDY, entry \"replacepamfiles\"." >> $mandy_dir/logs/log.txt
+	echo "Replace pam files via MANDY, entry \"replacepam\"." >> $mandy_dir/logs/log.txt
 	echo "Commands Executed: cp" >> $mandy_dir/logs/log.txt
 	echo >> $mandy_dir/logs/log.txt
 }
